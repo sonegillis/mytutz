e0d40c38697ba38b373675de2a115a4502c057be
Sone Gillis <sone_gillis@yahoo.com>
2019-09-08 19:04:42 +0100

    commit

--- mytutz/settings.py
+++ mytutz/settings.py
@@ -140,9 +140,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
